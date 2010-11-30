be327b365773b8add3a77cc5aca3f6372356a9a5
Fabio Napoleoni <f.napoleoni@gmail.com>
2010-11-30 14:45:05 +0000

    Application resource per email errors notifier

---
+++ library/Zle/Application/Resource/Notifier.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Zend Library Extension
+ *
+ * PHP version 5
+ *
+ * @category Zle
+ * @package  Zle_Application
+ * @author   Fabio Napoleoni <f.napoleoni@gmail.com>
+ * @license  http://framework.zend.com/license/new-bsd New BSD License
+ * @link     http://framework.zend.com/
+ */
+
+/**
+ * Zle_Application_Resource_Notifier
+ *
+ * @category Zle
+ * @package  Zle_Application
+ * @author   Fabio Napoleoni <f.napoleoni@gmail.com>
+ * @license  http://framework.zend.com/license/new-bsd New BSD License
+ * @link     http://framework.zend.com/
+ */
+class Zle_Application_Resource_Notifier extends Zend_Application_Resource_ResourceAbstract
+{
+    /**
+     * @var Zend_Log
+     */
+    private $_log;
+
+    /**
+     * @var Zle_Log_Writer_Mail
+     */
+    private $_mailWriter;
+
+    /**
+     * Defined by Zend_Application_Resource_Resource
+     *
+     * @return void
+     */
+    public function init()
+    {
+        if (!$this->_mailWriter) {
+            $this->_mailWriter = new Zle_Log_Writer_Mail($this->getOptions());
+            $this->getLog()->addWriter($this->_mailWriter);
+        }
+        return $this->_mailWriter;
+    }
+
+    /**
+     * Return the log plugin resource
+     *
+     * @throws Zend_Application_Resource_Exception if log is not configured
+     * using plugin resource syntax (i.e. via application.ini)
+     * @return Zend_Log
+     */
+    protected function getLog()
+    {
+        if (!$this->_log) {
+            /** @var $bootstrap Zend_Application_Bootstrap_Bootstrap */
+            $bootstrap = $this->getBootstrap();
+            // check for log availability
+            if (!$bootstrap->hasPluginResource('log')) {
+                throw new Zend_Application_Resource_Exception(
+                    'This resource depends on Zend_Application_Resource_Log'
+                );
+            }
+            // bootstrap log resource
+            $this->_log = $bootstrap->bootstrap('log')->getResource('log');
+        }
+        return $this->_log;
+    }
+}
---
+++ tests/Zle/Application/Resource/NotifierTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * Zend Library Extension
+ *
+ * PHP version 5
+ *
+ * @category Zle
+ * @package  Zle_Test
+ * @author   Fabio Napoleoni <f.napoleoni@gmail.com>
+ * @license  http://framework.zend.com/license/new-bsd New BSD License
+ * @link     http://framework.zend.com/
+ */
+
+/**
+ * NotifierTest
+ *
+ * @category Zle
+ * @package  Zle_Test
+ * @author   Fabio Napoleoni <f.napoleoni@gmail.com>
+ * @license  http://framework.zend.com/license/new-bsd New BSD License
+ * @link     http://framework.zend.com/
+ */
+class NotifierTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $logOptions = array(
+        'resources' => array(
+            'log' => array(
+                'stream' => array(
+                    'writerName' => 'Stream',
+                    'writerParams' => array(
+                        'stream' => "php://memory",
+                        'mode' => 'a'
+                    ),
+                    'filterName' => 'Priority',
+                    'filterParams' => array(
+                        'priority' => '4'
+                    ),
+                ),
+            ),
+        ),
+    );
+    
+    protected $resourceOptions = array(
+        'addresses' => 'foo@example.org',
+    );
+
+    protected function setUp()
+    {
+        // initialize an app and a bootstrap
+        $this->application = new Zend_Application('testing');
+        $this->bootstrap = new Zend_Application_Bootstrap_Bootstrap($this->application);
+    }
+
+    public function testResourceDependsOnLogResource()
+    {
+        try {
+            $resource = new Zle_Application_Resource_Notifier();
+            $resource->setBootstrap($this->bootstrap);
+            $resource->setOptions($this->resourceOptions);
+            $resource->init();
+            $this->fail();
+        } catch (Exception $e) {
+            $this->assertContains(
+                'depends on Zend_Application_Resource_Log',
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function testResourceIsInitializedWhenParametersAreSet()
+    {
+        $this->bootstrap->setOptions($this->logOptions);
+        $resource = new Zle_Application_Resource_Notifier();
+        $resource->setBootstrap($this->bootstrap);
+        $resource->setOptions($this->resourceOptions);
+        $this->assertTrue($resource->init() instanceof Zle_Log_Writer_Mail);
+    }
+
+    public function testLogHasOneMoreWriterAfterResourceInit()
+    {
+        $this->bootstrap->setOptions($this->logOptions);
+        $resource = new Zle_Application_Resource_Notifier();
+        $resource->setBootstrap($this->bootstrap);
+        $resource->setOptions($this->resourceOptions);
+        $resource->init();
+        $log = $resource->getBootstrap()->getResource('log');
+        $this->assertContains(
+            'Zle_Log_Writer_Mail',
+            var_export($log, true),
+            'Log object should contain Zle_Log_Writer_Mail as Writer'
+        );
+    }
+}
