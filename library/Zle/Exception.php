cade5a4e0b174fd723f9780b912a474322a8e99f
Fabio Napoleoni <f.napoleoni@gmail.com>
2011-01-13 16:02:57 +0000

    Aggiunta classe Zle_Exception

---
+++ library/Zle/Exception.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Zend Library Extension
+ *
+ * PHP version 5
+ *
+ * @category Zle
+ * @package  Zle_Exception
+ * @author   Fabio Napoleoni <f.napoleoni@gmail.com>
+ * @license  http://framework.zend.com/license/new-bsd New BSD License
+ * @link     http://framework.zend.com/
+ */
+
+/**
+ * Zle_Exception
+ *
+ * @category Zle
+ * @package  Zle_Exception
+ * @author   Fabio Napoleoni <f.napoleoni@gmail.com>
+ * @license  http://framework.zend.com/license/new-bsd New BSD License
+ * @link     http://framework.zend.com/
+ */
+class Zle_Exception extends Exception
+{
+
+}
